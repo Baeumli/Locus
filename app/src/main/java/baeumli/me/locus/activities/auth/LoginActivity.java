6d6f7a839a153a73b9ef8b96e625678f83ea4167
Luca Baumgartner <luca.baumgartner@stud.bbbaden.ch>
2018-12-12 22:35:05 +0100

    Added Splash Screen and App Icon

--- app/src/main/java/baeumli/me/locus/activities/auth/LoginActivity.java
+++ app/src/main/java/baeumli/me/locus/activities/auth/LoginActivity.java
@@ -1,11 +1,16 @@
 package baeumli.me.locus.activities.auth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import baeumli.me.locus.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView lblCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +18,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        lblCreateAccount = findViewById(R.id.lblCreateAccount);
 
+        lblCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getRegisterActivityIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(getRegisterActivityIntent);
+            }
+        });
 
     }
+
 }
--- app/src/main/java/baeumli/me/locus/activities/auth/RegisterActivity.java
+++ app/src/main/java/baeumli/me/locus/activities/auth/RegisterActivity.java
@@ -1,4 +1,31 @@
 package baeumli.me.locus.activities.auth;
 
-public class RegisterActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import baeumli.me.locus.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView lblCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        lblCreateAccount = findViewById(R.id.lblCreateAccount);
+
+        lblCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
 }
